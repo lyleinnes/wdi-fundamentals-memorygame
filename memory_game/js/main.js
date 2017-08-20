ad9f23a9d357305ee01c61647b172186724f902a
lyle innes <lyle.innes@gmail.com>
2017-08-20 18:03:17 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,29 @@
  console.log("Up and running!");
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+       rank: "queen",
+       suit: "hearts",
+       cardImage: "images/queen-of-hearts.png"
+	},
+	{
+       rank: "queen",
+       suit: "diamonds",
+       cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+       rank: "king",
+       suit: "hearts",
+       cardImage: "images/king-of-hearts.png"
+	},
+	{
+       rank: "king",
+       suit: "diamonds",
+       cardImage: "images/king-of-diamonds.png"
+	}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -10,17 +32,19 @@ var checkForMatch = function() {
     	console.log("You found a match!");
   		}
   		else {
-    	console.log("Sorry, try again.");
+    	alert("Sorry, try again.");
   		}
 	}
 }
 
 
 var flipCard = function(cardId) {
-	console.log ("User flipped " + cards[cardId] + ".");
-	cardsInPlay.push(cards[cardId]);
+	console.log ("User flipped " + cards[cardId].rank + ".");
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 	checkForMatch();
 }
 
 flipCard(0);
-flipCard(1);
+flipCard(2);
